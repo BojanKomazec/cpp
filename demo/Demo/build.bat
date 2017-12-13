bcd468d167a7849f72259efe66681a47bd873bd2
Bojan Komazec <bojan.komazec@gmail.com>
2017-12-13 12:19:07 +0000

    Windows config: paths updated to VS2017

--- demo/Demo/build.bat
+++ demo/Demo/build.bat
@@ -1,10 +1,10 @@
 @echo off
 pushd %cd%
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 
+call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 10.0.16299.0
 popd
 echo %cd% 
 REM optional: /Wall
 set compilerflags=/Od /Zi /EHsc
 set linkerflags=/OUT:demo.exe
-cl.exe %compilerflags% *.cpp stl/*.cpp stl/containers/*.cpp /link %linkerflags%
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.12.25827\bin\Hostx64\x64\cl.exe" %compilerflags% *.cpp stl/*.cpp stl/containers/*.cpp /link %linkerflags%
 echo Build completed.
